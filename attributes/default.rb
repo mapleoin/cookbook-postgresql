sous-chefs/postgresql

[#COOK-4316] - fix location of sysconfig on SUSE

attributes/default.rb
@@ -100,6 +100,7 @@ when "redhat", "centos", "scientific", "oracle"
 
   default['postgresql']['version'] = "8.4"
   default['postgresql']['dir'] = "/var/lib/pgsql/data"
+  default['postgresql']['sysconfdir'] = "/etc/sysconfig/pgsql/postgresql"
 
   if node['platform_version'].to_f >= 6.0 && node['postgresql']['version'] == '8.4'
     default['postgresql']['client']['packages'] = %w{postgresql-devel}
@@ -128,6 +129,7 @@ when "suse"
   end
 
   default['postgresql']['dir'] = "/var/lib/pgsql/data"
+  default['postgresql']['sysconfig'] = "/etc/sysconfig/postgresql"
   default['postgresql']['client']['packages'] = %w{postgresql-devel}
   default['postgresql']['server']['packages'] = %w{postgresql-server}
   default['postgresql']['contrib']['packages'] = %w{postgresql-contrib}

mapleoin/cookbook-postgresql

attributes/default.rb
@@ -100,6 +100,7 @@ when "redhat", "centos", "scientific", "oracle"
 
   default['postgresql']['version'] = "8.4"
   default['postgresql']['dir'] = "/var/lib/pgsql/data"
+  default['postgresql']['sysconfig'] = "/etc/sysconfig/pgsql/postgresql"
 
   if node['platform_version'].to_f >= 6.0 && node['postgresql']['version'] == '8.4'
     default['postgresql']['client']['packages'] = %w{postgresql-devel}
@@ -128,6 +129,7 @@ when "suse"
   end
 
   default['postgresql']['dir'] = "/var/lib/pgsql/data"
+  default['postgresql']['sysconfig'] = "/etc/sysconfig/postgresql"
   default['postgresql']['client']['packages'] = %w{postgresql-devel}
   default['postgresql']['server']['packages'] = %w{postgresql-server}
   default['postgresql']['contrib']['packages'] = %w{postgresql-contrib}